37998f7a5f6f7e66d7f1ccf0d0dd97fcfd19c2e9
nanoca031212 <lucasnatanael542@gmail.com>
2026-09-02 22:25:02 -0300

    track

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,3 +1,4 @@
-export const WHATSAPP_LINK = "https://wa.me/5531989210999";
+export const WHATSAPP_LINK =
+  "https://api.whatsapp.com/send/?phone=5531989210999&text=%F0%9F%9A%97%20Ol%C3%A1%2C%20preciso%20trocar%20a%20bateria%20do%20meu%20carro.%20Voc%C3%AAs%20podem%20me%20informar%20o%20pre%C3%A7o%20e%20se%20fazem%20entrega%20com%20instala%C3%A7%C3%A3o%3F&type=phone_number&app_absent=0";
 export const SITE_URL = "https://www.jfbaterias.com.br";
 export const SITE_NAME = "JF Baterias";
